9afb7a466e0d47a19986ced4f543a817962e49a2
Candace Wilson <candaceyw@gmail.com>
2020-03-01 22:24:29 -0500

    backend completed

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const config = require('config');
 const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/');
 
-const User = require('./models/User');
+const User = require('../models/User');
 
 
 // @route   GET api/auth
