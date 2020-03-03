0ff7eac05243955da49f51d7f435f0dc6e07d815
candaceyw <candaceyw@gmail.com>
2020-03-03 12:26:03 -0500

    contact components added

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Contacts from '../contacts/Contacts';
 
- const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
- export default Home
+const Home = () => {
+  return (
+    <div className='grid-2'>
+      <div>{/* Contact Form */}</div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
+};
+export default Home;
